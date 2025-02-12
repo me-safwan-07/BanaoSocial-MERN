cd0c7f4489ca090e1eb7eca80eec74457f7e9b93
me-safwan-07 <mesafwan07@gmail.com>
2025-02-12 23:09:59 +0530

    add helmet and morgan middleware for security and logging; integrate post routes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/database.js";
 import passport from "passport"; 
+import helmet from "helmet"
+import morgan from "morgan"
 import authRoutes from "./routes/auth.routes.js";
+import postRoutes from "./routes/post.routes.js";	
 
 const app = express();
 
@@ -10,10 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(passport.initialize());
-
-
+app.use(helmet());
+app.use(morgan("combined")); // Logging
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
